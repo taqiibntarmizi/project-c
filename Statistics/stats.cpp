798c2662cacce6f63c75e3eba1042cbaf43068a5
Taqiuddin Tarmidi <muhdtaqi916@gmail.com>
2025-02-03 21:45:20 -0500

    Add files via upload

---
+++ Statistics/stats.cpp
@@ -0,0 +1,46 @@
+// stats.cpp
+#include "stats.hpp"
+#include <cassert>
+#include <vector>
+#include <algorithm> // sort
+#include <cmath> // sqrt, modf
+
+using namespace std;
+
+int count(vector<double> v) {
+  assert(false);
+}
+
+double sum(vector<double> v) {
+  assert(false);
+}
+
+double mean(vector<double> v) {
+  assert(false);
+}
+
+double median(vector<double> v) {
+  assert(false);
+}
+
+double min(vector<double> v) {
+  assert(false);
+}
+
+double max(vector<double> v) {
+  assert(false);
+}
+
+double stdev(vector<double> v) {
+  assert(false);
+}
+
+double percentile(vector<double> v, double p) {
+  assert(false);
+}
+
+vector<double> filter(vector<double> v,
+                      vector<double> criteria,
+                      double target) {
+  assert(false);
+}
